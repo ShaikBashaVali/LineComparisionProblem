e506274fcfeb8743ed421ab3b5e483db7cf083fd
BashaVali <bashavali222@gmail.com>
2023-02-18 07:44:02 +0530

    [Basha]Add:CalculateLengthofLine

---
+++ LineComparisionProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using LineComparisionProblem;
+
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Line Comparison Program");
+            CalculateLeght calculateLeght = new CalculateLeght(1, 4, 2, 8);
+            calculateLeght.LenghtOfLine();
+            Console.ReadLine();
+        }
+    }
+}
